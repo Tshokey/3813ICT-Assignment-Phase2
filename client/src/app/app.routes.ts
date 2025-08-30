aabdba8a3d83fda47aeae568ce3dd5ef86107bef
Sonam Choki <tshokeysonamchoki7@gmail.com>
2025-08-30 15:32:03 +1000

    Add navigation to the app components by modifying app.routes.ts and app.ts files

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,33 @@
 import { Routes } from '@angular/router';
+import { Login } from './components/login/login';
+import { Register } from './components/register/register';
+import { Dashboard } from './components/dashboard/dashboard';
+import { Admins } from './components/admins/admins';
+import { Groups } from './components/groups/groups';
+import { Channels } from './components/channels/channels';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: Dashboard,
+    }, {
+        path: 'login',
+        component: Login,
+    },
+    {
+        path: 'register',
+        component: Register,
+    },
+    {
+        path: 'groups',
+        component: Groups,
+    },
+    {
+        path: 'channels',
+        component: Channels,
+    },
+    {
+        path: 'admins',
+        component: Admins,
+    }
+];
