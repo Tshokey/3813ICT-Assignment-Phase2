1fc0e195e6952bb86e17e6474af840f42b40c6ba
Sonam Choki <tshokeysonamchoki7@gmail.com>
2025-08-30 14:26:27 +1000

    Add base components; register, login, admins, groups, channels and dashboard

---
+++ client/src/app/components/admins/admins.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admins',
+  imports: [],
+  templateUrl: './admins.html',
+  styleUrl: './admins.css'
+})
+export class Admins {
+
+}
