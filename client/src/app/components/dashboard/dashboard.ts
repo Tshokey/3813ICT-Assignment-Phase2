4dcba3f97b34c4afc0b6c9f0b335e4c9fa541c4e
Sonam Choki <tshokeysonamchoki7@gmail.com>
2025-09-01 23:10:28 +1000

    Integrate the correct display and link, corresponding to the users

--- client/src/app/components/dashboard/dashboard.ts
+++ client/src/app/components/dashboard/dashboard.ts
@@ -1,11 +1,36 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../../services/auth-service';
+import { RouterLink, RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { User } from '../../models/user';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  imports: [RouterLink, CommonModule, RouterModule],
   templateUrl: './dashboard.html',
-  styleUrl: './dashboard.css'
+  styleUrls: ['./dashboard.css']
 })
 export class Dashboard {
 
+  constructor(private auth: AuthService) {}
+
+  get user(): User | null {
+    return this.auth.getCurrentuser();
+  }
+    
+  get isLoggedIn(): boolean{
+    return this.auth.isloggedIn();
+  }
+
+  logout() {
+    this.auth.logout();
+  }
+
+  get isSuperAdmin(){
+    return this.auth.hasRole('SUPER_ADMIN');
+  }
+
+  get isGroupAdmin(){
+    return this.auth.hasRole('GROUP_ADMIN');
+  }
 }
