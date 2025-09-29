0cc0b2cb48c938c56f3f7479c0f552f71dda763c
Sonam Choki <tshokeysonamchoki7@gmail.com>
2025-09-29 22:53:11 +1000

    Update server to integrate socket

--- client/server/server.js
+++ client/server/server.js
@@ -3,9 +3,14 @@ const cors = require("cors");
 const fs = require("fs");
 const path = require("path");
 const app = express();
+const http = require("http");
+const socketIo = require("socket.io");
+const sockets = require("./socket.js");
+const listen = require("./listen.js");
 
 app.use(cors());
 app.use(express.json());
+const server = http.createServer(app);
 
 const DATA_FILE = path.join(__dirname, "data", "data.json");
 
@@ -58,8 +63,17 @@ app.use("/api/auth", authRoutes(appData, saveData))
 app.use("/api/groups", groupRoutes(appData, saveData))
 app.use("/api/channels", channelRoutes(appData, saveData))
 
-const PORT = process.env.PORT || 3000
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-  console.log("Data loaded from JSON file on startup")
-})
+const PORT = process.env.PORT || 3000;
+
+const options = {
+    cors: {
+        origin:'http://localhost:4200',
+        methods:['GET','POST'],
+    }
+};
+const io = socketIo(server, options);
+
+
+sockets.connect(io, PORT);
+
+listen.listen(server, PORT);
